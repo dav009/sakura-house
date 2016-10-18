5eef2f62e56e67006cb983c12782994499dddc2c
David Przybilla <dav.alejandro@gmail.com>
2016-10-18 17:27:34 +0700

    cli tool

--- sakura/__main__.py
+++ sakura/__main__.py
@@ -7,23 +7,25 @@ import json
 
 @click.command()
 @click.argument('action')
-@click.option('--move_date', help='expected date to movie in')
+@click.option('--move_date', help='expected date to movie in. i.e: 2016/10/28')
 @click.option('--output', help='output folder for extracted data')
 def main(action, move_date, output):
 
     if action == "dump":
-        filename = "sakura-%s.json" % (move_date)
-        print("dumping data: %s") % (filename)
+        output_path = "sakura-%s.json" % (move_date.replace("/", "-"))
+        print("dumping data: %s" % output_path)
         data = scraper.get_sakura_data(move_in_date=move_date)
-        out = codecs.open(filename, 'w', 'utf-8')
+        out = codecs.open(output_path, 'w', 'utf-8')
         out.write(json.dumps(data))
         out.close()
-        print("successfully dumped data at: %s" % filename)
+        print("successfully dumped data at: %s" % output_path)
     elif action == "server":
         server.run()
 
     else:
         print("please use a valid command.")
+        click.echo(click.get_current_context().get_help()) 
+        
 
 
 if __name__ == "__main__":
